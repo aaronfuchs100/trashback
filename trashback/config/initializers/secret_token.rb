32d9d8933a295ae06d376d7b38cdc887756b5aee
Aaron Fuch <aaronfuchs100@gmail.com>
2012-12-24 17:26:31 +0200

    initial commit

---
+++ trashback/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Trashback::Application.config.secret_token = '320b0622afb70a2cd69efe216612c702e359a40d639b095ba851877bbbaa49df81b6f2422baa45893e738b3d785651d343794ac28c8acf38125314316f86cdf6'
